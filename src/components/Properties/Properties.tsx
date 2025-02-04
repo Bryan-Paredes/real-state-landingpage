af3186c714730d5310f319e86b1c46d372303ea2
BryanParedes <bryansparedes2@gmail.com>
2025-02-03 21:37:37 -0600

    Añade botón de "me gusta" en el componente Properties, mejora el diseño y ajusta la estructura del enlace para una mejor responsividad.

--- src/components/Properties/Properties.tsx
+++ src/components/Properties/Properties.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import Link from "next/link";
@@ -10,8 +9,10 @@ import {
   LiaRulerCombinedSolid,
   LiaStarSolid,
 } from "react-icons/lia";
+import { FaRegHeart } from "react-icons/fa";
 import { formatPrice } from "@/utils/formatPrice";
 import { useState } from "react";
+import Image from "next/image";
 
 export function Properties() {
   const [counterHouses, setCounterHouses] = useState(8);
@@ -38,45 +39,58 @@ export function Properties() {
             star,
             meters,
           }) => (
-            <Link
+            <div
               key={id}
-              href={`/properties/${id}`}
-              className="shadow-light hover:shadow-2xl rounded-2xl transition-all duration-300 cursor-pointer"
+              className=" hover:shadow-md hover:shadow-secondary/50 hover:scale-[1.02] rounded-2xl transition-all duration-300 cursor-pointer"
             >
               <div className="relative -z-[1]">
                 <div className="relative">
-                  <div className="absolute flex items-center px-2 py-1 rounded-lg bg-slate-50 top-2 right-2 text-secondary">
-                    <LiaStarSolid />
-                    <span className="ml-1 font-semibold">{star}</span>
+                  <div className="absolute flex items-center top-2 right-2  z-10">
+                    <button
+                      className="text-red-600 hover:text-red-300 transition-colors duration-300 cursor-pointer"
+                      onClick={() => console.log("Like")}
+                    >
+                      <FaRegHeart size={24} color={""} />
+                    </button>
                   </div>
-                  <img
+                  <Image
                     src={`${images ? images[0] : "/assets/404-not-found.jpg"}`}
                     alt={location}
                     width={150}
                     height={150}
-                    className="object-cover w-full min-w-full h-[200px] rounded-t-2xl"
+                    className="object-cover w-full min-w-full h-[200px] rounded-xl"
                   />
-                  <div className="px-3 py-5">
-                    <p className="text-secondary">{location}</p>
-                    <p className="font-semibold">{formatPrice(price)}</p>
-                    <div className="gap-4 mt-2 xl:flex">
-                      <div className="flex items-center justify-center px-2 py-1 rounded-lg my-2 bg-slate-300/30">
-                        <LiaBedSolid />
-                        <span className="ml-2">{bedrooms}</span>
+                  <Link href={`/properties/${id}`} className="cursor-pointer">
+                    <div className="px-3 py-5 flex flex-col gap-2 items-stretch justify-start">
+                      <div className="flex items-center justify-between">
+                        <div className="flex flex-col gap-2">
+                          <p className="text-secondary">{location}</p>
+                          <p className="font-semibold">{formatPrice(price)}</p>
+                        </div>
+                        <div className=" flex items-center justify-center px-2 py-1 rounded-lg bg-secondary/50 top-5 right-2 text-white">
+                          <LiaStarSolid />
+                          <span className="ml-1 font-semibold">{star}</span>
+                        </div>
                       </div>
-                      <div className="flex items-center justify-center px-2 py-1 rounded-lg my-2 bg-slate-300/30">
-                        <LiaBathSolid />
-                        <span className="ml-2">{bathroom}</span>
-                      </div>
-                      <div className="flex items-center justify-center px-2 py-1 rounded-lg my-2 bg-slate-300/30">
-                        <LiaRulerCombinedSolid />
-                        <span className="ml-2">{meters}</span>
+                      <div className="gap-4 mt-2 xl:flex">
+                        <div className="flex items-center justify-center px-2 py-1 rounded-lg my-2 bg-slate-300/30">
+                          <LiaBedSolid />
+                          <span className="ml-2">{bedrooms}</span>
+                        </div>
+                        <div className="flex items-center justify-center px-2 py-1 rounded-lg my-2 bg-slate-300/30">
+                          <LiaBathSolid />
+                          <span className="ml-2">{bathroom}</span>
+                        </div>
+                        <div className="flex items-center justify-center px-2 py-1 rounded-lg my-2 bg-slate-300/30">
+                          <LiaRulerCombinedSolid />
+                          <span className="ml-2">{meters}</span>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </Link>
                 </div>
               </div>
-            </Link>
+            </div>
           )
         )}
       </div>
