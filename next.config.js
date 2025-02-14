e054fa1ea29712ad081749fd993d97007c87fd96
BryanParedes <bryansparedes2@gmail.com>
2025-02-14 13:32:06 -0600

    Elimina componentes y tipos no utilizados para limpiar el código y mejorar la estructura del proyecto.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "backend-real-state-fr3m.onrender.com",
-        port: "",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- src/components/SearchButton/SearchButton.tsx
+++
@@ -1,9 +0,0 @@
-import { RiSearch2Line } from "react-icons/ri";
-
-export function SearchButton() {
-  return (
-    <div className="flex items-center justify-center p-4 text-white rounded-full  bg-secondary cursor-pointer">
-      <RiSearch2Line size={30} />
-    </div>
-  );
-}
