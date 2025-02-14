a219cfbb5aa312ce2a47fce7f3e4a32df88c0f9a
BryanParedes <bryansparedes2@gmail.com>
2025-02-14 13:30:07 -0600

    Añade nuevos datos para el encabezado, propiedades, servicios y slider; incluye información sobre categorías, casas y servicios ofrecidos.

---
+++ src/data/header.ts
@@ -0,0 +1,24 @@
+import { HeaderData } from "@/types/header";
+
+export const dataHeader: HeaderData[] = [
+    // {
+    //   id: 1,
+    //   name: "Home",
+    //   link: "/",
+    // },
+    {
+        id: 5,
+        name: "Categorias",
+        link: "/#categories",
+    },
+    {
+        id: 3,
+        name: "Servicios",
+        link: "/#services",
+    },
+    {
+        id: 2,
+        name: "Nosotros",
+        link: "/#about",
+    },
+];
