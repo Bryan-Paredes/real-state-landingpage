fcd2cc261331f5ad96240cf287df31db66bccd51
BryanParedes <bryansparedes2@gmail.com>
2025-02-14 13:38:08 -0600

    Elimina propiedades no utilizadas del componente Form y actualiza el enlace de llamada para mejorar la funcionalidad y la claridad del código.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,7 @@
 import Link from "next/link";
-import { dataProperties } from "../Properties/Properties.data";
-import { PropertyProps } from "../Property/Property.types";
 
 /* eslint-disable @next/next/no-img-element */
-export function Form({ house }: PropertyProps) {
+export function Form() {
   return (
     <div className="px-3">
       <div className="py-4 px-3 rounded-lg shadow-2xl">
@@ -61,7 +59,7 @@ export function Form({ house }: PropertyProps) {
               Enviar mensaje
             </button>
             <button className="border-[1px] border-secondary text-secondary px-4 py-2 rounded-lg text-sm hover:bg-black transition-all duration-300 hover:text-white hover:border-black">
-              <Link href={`tel:${house.phone}`}>Llamar</Link>
+              <Link href={`tel:+34665656565`}>Llamar</Link>
             </button>
           </div>
         </div>
